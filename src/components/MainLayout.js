0a0d77a7de4d0cabd92029d7ac5ddc69741a4a1a
Newton Ombese <newtonombese1@gmail.com>
2023-02-14 09:07:06 +0300

    adding a mainlayout component to ease the work to be done when importing footer and header comps

--- src/components/MainLayout.js
+++ src/components/MainLayout.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Footer from './Footer'
 import Header from './Header'
 
-function MainLayout({children}) {
+function MainLayout({ children }) {
   return (
     <div>
         <Header />
