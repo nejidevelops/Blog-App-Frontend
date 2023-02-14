f0ebf59a47ed4d952b706da72ec0aa80470736a7
Newton Ombese <newtonombese1@gmail.com>
2023-02-14 08:28:18 +0300

    Getting started on my Blog Application

---
+++ src/components/MainLayout.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Footer from './Footer'
+import Header from './Header'
+
+function MainLayout({children}) {
+  return (
+    <div>
+        <Header />
+        {children}
+        <Footer />
+    </div>
+  )
+}
+
+export default MainLayout
