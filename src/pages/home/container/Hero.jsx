ae25bab0d7c24c6090233ecff3993754cc0ffbbe
Newton Ombese <newtonombese1@gmail.com>
2023-04-21 19:15:05 +0300

    :art:feat: Working on the hero section of the project

--- src/pages/home/container/Hero.jsx
+++ src/pages/home/container/Hero.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FiSearch } from 'react-icons/fi'
 import { images } from '../../../constants'
 
 const Hero = () => {
@@ -8,17 +9,18 @@ const Hero = () => {
             <h1 className='font-roboto text-3xl text-center font-bold text-dark-soft md:text-5xl lg:text-left lg:max-w-[540px]'>Read the most interesting articles</h1>
             <p className='text-dark-light mt-4 text-center md:text-xl lg:text-left'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas a, fugiat praesentium ratione illum necessitatibus optio est impedit dignissimos, architecto placeat rem omnis totam sunt, tempore</p>
             <div className='flex flex-col gap-y-2.5 mt-10 relative'>
-                <div>
-                    <input type='text' placeholder='Search article'/>
+                <div className='relative'>
+                    <FiSearch className='absolute left-3 top-1/2 -translate-y-1/2 w-6 h-6 text-[#959EAD]'/>
+                    <input className='placeholder:font-bold font-semibold text-dark-soft placeholder:text-[#959EAD] rounded-lg pl-12 pr-3 w-full py-3 focus:outline-none shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px] md:py-4' type='text' placeholder='Search article'/>
                 </div>
-                <button>Search</button>
+                <button className='w-full bg-primary text-white font-semibold rounded-lg px-5 py-3 md:absolute md:right-2 md:top-1/2 md:-translate-y-1/2 md:w-fit md:py-2'>Search</button>
             </div>
-            <div>
-                <span>Popular Tags:</span>
-                <ul>
-                    <li>Design</li>
-                    <li>User Experience</li>
-                    <li>User Interfaces</li>
+            <div className='flex mt-4 flex-col lg:flex-row lg:flex-nowrap lg:gap-x-4 lg:mt-7'>
+                <span className='text-dark-light font-semibold italic'>Popular Tags:</span>
+                <ul className='flex flex-wrap gap-x-2.5 gap-y-2.5 mt-3'>
+                    <li className='rounded-lg bg-primary bg-opacity-10 px-3 py-1.5 text-primary font-semibold'>Design</li>
+                    <li className='rounded-lg bg-primary bg-opacity-10 px-3 py-1.5 text-primary font-semibold'>User Experience</li>
+                    <li className='rounded-lg bg-primary bg-opacity-10 px-3 py-1.5 text-primary font-semibold'>User Interfaces</li>
                 </ul>
             </div>
         </div>
