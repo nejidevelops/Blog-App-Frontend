b77f158e11bd32c786cc2f70f0a6bdeb87def1c1
Newton Ombese <newtonombese1@gmail.com>
2023-04-25 10:18:03 +0300

    :art:feat:Working on the articles section

---
+++ src/pages/home/container/Articles.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ArticleCard from '../../../components/ArticleCard'
+
+const Articles = () => {
+  return (
+    <section className='container mx-auto flex flex-wrap md:gap-x-5 gap-y-5 px-5 py-10'>
+        <ArticleCard />
+    </section>
+  )
+}
+
+export default Articles
