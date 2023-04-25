70c06ef8d4940908fb2ab7f14001e3e5636eeccf
Newton Ombese <newtonombese1@gmail.com>
2023-04-25 10:17:47 +0300

    :art:feat:Importing Articles page to the homepage

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import MainLayout from '../../components/MainLayout';
 import Hero from './container/Hero';
+import Articles from './container/Articles';
 
 function HomePage() {
   return (
     <MainLayout>
       <Hero />
+      <Articles />
     </MainLayout>
   )
 }
