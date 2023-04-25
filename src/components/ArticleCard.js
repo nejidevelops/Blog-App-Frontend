204f2217c7cffb7adee0d267cb5af338b3f081c3
Newton Ombese <newtonombese1@gmail.com>
2023-04-25 10:32:08 +0300

    :art::lipstick:feat: Working on the card

--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import { images } from '../constants'
 
-const ArticleCard = () => {
+const ArticleCard = ({ className }) => {
   return (
-    <div>
-      
+    <div className={`rounded-xl overflow-hidden shadow-[rgba(7,_65,_210,_0.1)_0px_9px_30px] ${className}`}>
+      <img src={images.Post1Image} alt="title" className='w-full object-cover object-center h-auto' />
     </div>
   )
 }
